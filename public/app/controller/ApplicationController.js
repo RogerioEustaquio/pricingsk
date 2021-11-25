e193a351aec316d465a7365cd17517d199b47708
Rogerio Eustaquio Dos Santos <rogeriosantos.adm@jspecas.com.br>
2021-11-25 16:55:27 -0300

    usuarios.

--- public/app/controller/ApplicationController.js
+++ public/app/controller/ApplicationController.js
@@ -103,7 +103,7 @@ Ext.define('App.controller.ApplicationController', {
             viewportTabs = viewport.down('#applicationtabs'),
             tab = viewportTabs.down(xtype);
 
-        var acessos = ['EVERTON','ROGERIOADM','RENATO','PEDROOPE'];
+        var acessos = ['EVERTON','ROGERIOADM','RENATO','PEDROOPE','MAYKONRS','WELISONOPE'];
 
         if(acessos.indexOf(USUARIO.usuarioSistema) === -1){
 
