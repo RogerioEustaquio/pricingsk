b50999c83a1a82da5bb3b07dc5895d10b626a05d
Rogerio Eustaquio Dos Santos <rogeriosantos.adm@jspecas.com.br>
2022-01-05 12:00:19 -0300

    usuario

--- public/app/controller/ApplicationController.js
+++ public/app/controller/ApplicationController.js
@@ -103,7 +103,7 @@ Ext.define('App.controller.ApplicationController', {
             viewportTabs = viewport.down('#applicationtabs'),
             tab = viewportTabs.down(xtype);
 
-        var acessos = ['EVERTON','ROGERIOADM','RENATO','PEDROOPE','MAYKONRS','WELISONOPE','EVERTONOPE','JOSECARLOS'];
+        var acessos = ['EVERTON','ROGERIOADM','RENATO','PEDROOPE','MAYKONRS','WELISONOPE','EVERTONOPE','JOSECARLOS','ESTEFANIA'];
 
         if(acessos.indexOf(USUARIO.usuarioSistema) === -1){
 
